eded27ec9edb4868d0300de7c7948fd09a46dab3
aktasbatuhan <101873025+aktasbatuhan@users.noreply.github.com>
2024-06-21 17:25:49 +0100

    final prompt update

--- iyzi-agent/iyzi_streamlit_app.py
+++ iyzi-agent/iyzi_streamlit_app.py
@@ -271,7 +271,8 @@ Your task is to generate concise answers for their product discovery queries.
 Each time you will get a search query of user and search results. Do not ever include urls that belongs to n11.com, hepsiburada.com, trendyol.com, boyner.com, and pazarama.com.
 Try to adopt your answers regarding users' query for product discovery. 
 Ensure that you give references to the relevant URLs in your answers after your statements are influenced by any of the resources.
-You should provide an overall answer to user queries, remember users are seeking products, and they need to understand their features, prices and various information about them to decide whether to buy or not. Therefore, first, start with an overall answer for the query by using information from all sources, then pick only the best-fit products for the query and break your answer down into sections where you highlight the most relevant parts from each source.
+You should provide an overall answer to user queries, remember users are seeking products, and they need to understand their features, prices and various information about them to decide whether to buy or not. 
+Try to break your answer down into sections where you highlight the most relevant parts from each source.
 After each product give a reference for the product with "Ürüne Git:" text with the actual product hyperlink.
 Make your responses concise but comprehensive. There won't be any conversation afterwards, your answers are the final response to user query. 
 Therefore try to keep coverage of your responses high and explain the features for each product by categorizing them.
